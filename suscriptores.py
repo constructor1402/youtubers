cfa89e3593e79439b456363230535baca76cc60d
constructor1402 <cristianestudiante15@gmail.com>
2023-09-10 18:26:03 -0500

    Actualización de suscriptores.py

--- suscriptores.py
+++ suscriptores.py
@@ -1,6 +1,10 @@
+from sklearn.preprocessing import StandardScaler
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Ridge
+import tensorflow as tf
+from tensorflow import keras, layers
 
 
 data_set_youtubers = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTjiJn06PS3re6UayHZOHqgDxqegCFAw5ouXeTbFP8sUPRyzC4bWbQPPRpO72NCvg/pubhtml"
@@ -15,8 +19,38 @@ y = df['subscribers']  # Variable objetivo
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42)
 
-# regresión lineal
-modelo_regresion = LinearRegression()
+# Crear un objeto de regresión Ridge
+modelo_regresion_ridge = Ridge(alpha=1.0)
 
-# Entrenar el modelo de regresión lineal en el conjunto de entrenamiento
-modelo_regresion.fit(X_train, y_train)
+# Entrenar el modelo de regresión Ridge en el conjunto de entrenamiento
+modelo_regresion_ridge.fit(X_train, y_train)
+
+# Red neuronales
+
+# Normalizar las características (opcional, pero a menudo es útil en redes neuronales)
+
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+# Crear el modelo de redes neuronales
+model = keras.Sequential([
+    layers.Dense(64, activation='relu', input_shape=(X_train.shape[1],)),
+    layers.Dense(32, activation='relu'),
+    layers.Dense(1)  # Capa de salida para regresión
+])
+
+# Compilar el modelo
+model.compile(optimizer='adam', loss='mean_squared_error', metrics=['mae'])
+
+# Entrenar el modelo
+history = model.fit(X_train, y_train, epochs=100,
+                    batch_size=32, validation_split=0.2, verbose=2)
+
+# Evaluar el modelo en el conjunto de prueba
+loss, mae = model.evaluate(X_test, y_test, verbose=0)
+print(f'Error cuadrático medio (MSE): {loss}')
+print(f'Error absoluto medio (MAE): {mae}')
+
+# Realizar predicciones
+y_pred = model.predict(X_test)
