e4936e82cba7c339f5a119598812dbbdf8f0c09d
Cristian Rodriguez <cristianrodriguez@MacBook-Pro-de-Adelayda.local>
2023-09-10 17:57:53 -0500

    Actualización de suscriptores.py

--- suscriptores.py
+++ suscriptores.py
@@ -1,4 +1,14 @@
-print("Hola")
-print("Hola2")
-print("Hola")
-print("Hola2")
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+data_set_youtubers = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTjiJn06PS3re6UayHZOHqgDxqegCFAw5ouXeTbFP8sUPRyzC4bWbQPPRpO72NCvg/pubhtml"
+
+df = pd.read_excel(data_set_youtubers)
+
+# características (variables independientes) y variable objetivo (variable dependiente)
+X = df[['video views', 'uploads', 'country_rank']]  # Características
+y = df['subscribers']  # Variable objetivo
+
+# Dividir los datos en conjuntos de entrenamiento y prueba (por ejemplo, 80% entrenamiento, 20% prueba)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42)
